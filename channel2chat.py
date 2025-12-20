ef73b64b504dc967e7c3810b3c54d894b75eb686
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-21 04:36:15 +0530

    Add files via upload

---
+++ channel2chat.py
@@ -0,0 +1,31 @@
+from telethon import TelegramClient, events
+
+api_id = 24474379
+api_hash = "e994452a74a2e0a053827d0f63e4c3b0"
+
+TARGET_GROUP = -1002303286535  # forum group ID
+
+CHANNEL_TOPIC_MAP = {
+    "NotNianticUpdates": 11079, # Ingress Updates topic ID
+    "IngressFSNews": 15,        # First Saturday topic ID
+    "NianticOfficial": 11079,   # Ingress Updates topic ID
+    "IUENG": 11079,   # Ingress Updates topic ID
+    "IWWC2025": 11079,  # Ingress Updates topic ID
+    "HackventscalendarNews": 11079, # Ingress Updates topic ID
+}
+
+client = TelegramClient("user_session", api_id, api_hash)
+
+@client.on(events.NewMessage(chats=list(CHANNEL_TOPIC_MAP.keys())))
+async def handler(event):
+    topic_id = CHANNEL_TOPIC_MAP.get(event.chat.username)
+
+    await client.send_message(
+        TARGET_GROUP,
+        event.message,
+        message_thread_id=topic_id
+    )
+
+client.start()
+print("✅ Forwarding to specific topics...")
+client.run_until_disconnected()
