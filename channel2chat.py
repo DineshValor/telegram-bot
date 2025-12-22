cf4ffcb5c6fe229d1ed6ef643c78d294b54afa47
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-22 15:30:23 +0530

    Update CHANNEL_TOPIC_MAP for topic IDs

--- channel2chat.py
+++ channel2chat.py
@@ -6,14 +6,18 @@ api_hash = "e982daed463e4c9826c6c9ba828c2c37"
 TARGET_GROUP = -1002303286535  # forum group ID
 
 CHANNEL_TOPIC_MAP = {
-    "NotNianticUpdates": 1, # Ingress Updates topic ID
+    "NotNianticUpdates": 1, # XFaction Chat topic ID
     "IngressFSNews": 15,        # First Saturday topic ID
     "NianticOfficial": 11079,   # Ingress Updates topic ID
-    "IUENG": 1,   # Ingress Updates topic ID
-    "IWWC2025": 1,  # Ingress Updates topic ID
-    "HackventscalendarNews": 1, # Ingress Updates topic ID
+    "IUENG": 1,   # XFaction Chat topic ID
+    "IWWC2025": 1,  # XFaction Chat topic ID
+    "HackventscalendarNews": 1, # XFaction Chat topic ID
     "IngressMissionAddicts": 8201, # Mission Banners topic ID
-    "dineshvalorchannel": 1, # Test Updates
+    "IEToday": 1, # XFaction Chat topic ID
+    "RGNNticker": 1, # XFaction Chat topic ID
+    "PasscodesIngress": 11079, # Ingress Updates topic ID
+    "IngressAuctions": 1, # XFaction Chat topic ID
+    "dineshvalorchannel": 1, # Test Updates (XFaction Chat) topic ID
 }
 
 client = TelegramClient("user_session", api_id, api_hash)
