2082ea7eacca72e4bff95d17b19a09c381f211e0
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-24 16:16:25 +0530

    Update startup.py

--- core/startup.py
+++ core/startup.py
@@ -1,8 +1,6 @@
 from core.client import client
-from db.database import init_db
 
 def start_bot():
-    init_db()
     client.start()
     print("🚀 Bot started successfully")
     client.run_until_disconnected()
