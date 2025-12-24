591477dafddeb152897b0dbbdc41092409cf20d4
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-24 14:49:59 +0530

    Create moderation.py

---
+++ config/moderation.py
@@ -0,0 +1,14 @@
+TOPIC_RULES = {
+    10: {
+        "text": False,
+        "photo": True,
+        "video": True,
+        "doc_ext": {".jpg", ".jpeg", ".png", ".mp4"},
+    },
+    8201: {
+        "text": False,
+        "photo": True,
+        "video": False,
+        "doc_ext": {".jpg", ".jpeg", ".png", ".zip", ".rar"},
+    },
+}
