b34f50c96ef2ee47073978a7124ed9367eab06c0
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-25 01:49:45 +0530

    Update update.sh

--- systemd/update.sh
+++ systemd/update.sh
@@ -3,20 +3,20 @@ set -e
 
 REPO_DIR="/opt/telegram-bot"
 SERVICE_NAME="telegram-bot"
+BRANCH="master"
 
 cd "$REPO_DIR"
 
 echo "[$(date)] Checking for updates..."
 
-# Fetch latest changes
 git fetch origin
 
 LOCAL=$(git rev-parse HEAD)
-REMOTE=$(git rev-parse origin/main)
+REMOTE=$(git rev-parse origin/$BRANCH)
 
 if [ "$LOCAL" != "$REMOTE" ]; then
     echo "[$(date)] Updates found. Pulling..."
-    git pull origin main
+    git pull origin $BRANCH
 
     echo "[$(date)] Restarting service..."
     systemctl restart $SERVICE_NAME
