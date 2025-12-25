d2cd749636a3d5ae4a9e5c150c9b32a20fe0f0aa
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-25 10:12:06 +0530

    Update forwarding.py

--- config/forwarding.py
+++ config/forwarding.py
@@ -1,30 +1,34 @@
 from telethon.tl.types import MessageMediaPhoto, MessageMediaDocument
 
 CHANNEL_TOPIC_MAP = {
-    -1001268572490: 1,
-    -1003341948290: 1,
-    -1001305415858: 15,
-    -1001008795454: 11079,
-    -1001170454563: 11079,
-    -1001126789733: 11079,
-    -1001075281753: 8201,
-    -1001078001228: 8201,
-    -1001420065662: 8201,
-    -1002105354149: 1,
-    -1001167466234: 1,
-    -1001077599821: 1,
-    -1001064978090: 1,
-    -1001402896020: 1,
-    -1001851154018: 1,
-    -1001003824281: 1,
-    -1001837191055: 1,
+    -1001268572490: 1, # Dinesh Valor Channel : XFaction Chat
+    -1003341948290: 1, # Ingress India Test : XFaction Chat
+    
+    -1001305415858: 15, # IngressFS Notifications : First Saturday
+    
+    -1001008795454: 11079, # Passcodes Ingress PRIME : Ingress Updates
+    -1001170454563: 11079, # Ingress : IngressIngress Updates
+    -1001126789733: 11079, # Ingress Passcodes : Ingress Updates
+    
+    -1001075281753: 8201, # Mission Banners, Oh My! (Global XFAC [ENG]) : Mission Banners
+    -1001078001228: 8201, # [Global] #MissionProject : Mission Banners
+    -1001420065662: 8201, # Ingress Mission Addicts : Mission Banners
+    
+    -1002105354149: 1, # Ingress World Wide Competition 2025 : XFaction Chat
+    -1001167466234: 1, # NotNiantic Updates : XFaction Chat
+    -1001077599821: 1, # Ingress Updates [ENG] : XFaction Chat
+    -1001064978090: 1, # RGNN Ticker : XFaction Chat
+    -1001402896020: 1, # News_Hackventscalendar : XFaction Chat
+    -1001851154018: 1, # Ingress.Plus : XFaction Chat
+    -1001003824281: 1, # Enlightened Today : XFaction Chat
+    -1001837191055: 1, # Ingress Auctions : XFaction Chat
 }
 
 MEDIA_ONLY_CHANNELS = {
-    -1003341948290,
-    -1001075281753,
-    -1001078001228,
-    -1001420065662,
+    -1003341948290, # Ingress India Test : XFaction Chat
+    -1001075281753, # Mission Banners, Oh My! (Global XFAC [ENG]) : Mission Banners
+    -1001078001228, # [Global] #MissionProject : Mission Banners
+    -1001420065662, # Ingress Mission Addicts : Mission Banners
 }
 
 ALLOWED_EXTENSIONS = {".jpeg", ".jpg", ".png", ".zip", ".rar"}
