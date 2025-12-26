94316d18911688940cca91076c88637eeef2596a
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-27 02:16:36 +0530

    Update moderation.py

--- config/moderation.py
+++ config/moderation.py
@@ -3,19 +3,23 @@ TOPIC_RULES = {
         "text": True,
         "photo": True,
         "video": True,
-        "doc_ext": None,  # None = allow all documents
+        "forwarded_allowed": True,   # allow all forwarded messages
+        "doc_ext": None,
     },
-    
-    10: { # BRAGGING RIGHTS
+
+    10: {  # BRAGGING RIGHTS
         "text": False,
         "photo": True,
         "video": True,
+        "forwarded_allowed": False,  # delete all forwarded messages
         "doc_ext": {".jpg", ".jpeg", ".png", ".mp4"},
     },
-    8201: { # Mission Banners
+
+    8201: {  # Mission Banners
         "text": False,
         "photo": True,
         "video": False,
+        "forwarded_allowed": None,   # follow normal rules
         "doc_ext": {".jpg", ".jpeg", ".png", ".zip", ".rar"},
     },
 }
