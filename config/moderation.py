90c05742f74f4a0ff6dc3bbd2dac66231d76089c
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-27 00:57:11 +0530

    Update moderation.py

--- config/moderation.py
+++ config/moderation.py
@@ -1,11 +1,11 @@
 TOPIC_RULES = {
-    10: {
+    10: { # BRAGGING RIGHTS
         "text": False,
         "photo": True,
         "video": True,
         "doc_ext": {".jpg", ".jpeg", ".png", ".mp4"},
     },
-    8201: {
+    8201: { # Mission Banners
         "text": False,
         "photo": True,
         "video": False,
