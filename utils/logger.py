5ec8e2ef43f293918a22a6b4ad9c8f2b3cd5cff6
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2025-12-29 03:34:41 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,10 +1,6 @@
 import logging
-import asyncio
 
-from utils.telegram_log_handler import TelegramLogHandler
-
-
-def setup_logger(loop: asyncio.AbstractEventLoop | None = None):
+def setup_logger():
     logger = logging.getLogger("telegram-bot")
     logger.setLevel(logging.INFO)
 
@@ -15,15 +11,8 @@ def setup_logger(loop: asyncio.AbstractEventLoop | None = None):
         "%(levelname)s | %(name)s | %(message)s"
     )
 
-    # Console handler (journalctl)
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
+
     logger.addHandler(console_handler)
-
-    # Telegram handler
-    if loop is not None:
-        tg_handler = TelegramLogHandler(loop)
-        tg_handler.setFormatter(formatter)
-        logger.addHandler(tg_handler)
-
     return logger
