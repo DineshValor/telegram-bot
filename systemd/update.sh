40165e2d7f1d868aa530a06721886c88a20c3426
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2026-01-01 02:10:03 +0530

    Update update.sh

--- systemd/update.sh
+++ systemd/update.sh
@@ -7,9 +7,6 @@ BRANCH="master"
 
 cd "$REPO_DIR"
 
-sudo chmod +x /home/ubuntu/telegram-bot/systemd/update.sh
-sudo chmod +x /home/ubuntu/telegram-bot/systemd/journal-watcher.sh
-
 # Ensure this is a git repo
 if [ ! -d ".git" ]; then
     echo "Not a git repository: $REPO_DIR"
