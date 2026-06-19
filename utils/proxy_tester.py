55df17edf30cda045793022a053ba124d63d3d46
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2026-06-19 13:05:43 +0530

    Update proxy_tester.py

--- utils/proxy_tester.py
+++ utils/proxy_tester.py
@@ -2,22 +2,26 @@ from telethon import TelegramClient
 from telethon.errors import RPCError
 
 async def test_proxy(api_id, api_hash, proxy):
+
     try:
         client = TelegramClient(
             "proxy_test",
             api_id,
             api_hash,
-            proxy=proxy,
-            connection_retries=1
+            proxy=(
+                proxy["server"],
+                proxy["port"],
+                proxy["secret"]
+            )
         )
 
         await client.connect()
 
-        if await client.is_user_authorized():
-            await client.disconnect()
-            return True
+        ok = await client.is_user_authorized()
+
+        await client.disconnect()
+
+        return ok
 
     except Exception:
-        pass
-
-    return False
+        return False
