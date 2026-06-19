9a1f1aedd3edffe442985549333a1800ef7ec585
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2026-06-19 12:42:06 +0530

    Enhance TelegramClient initialization with retries

--- core/client.py
+++ core/client.py
@@ -1,4 +1,11 @@
 from telethon import TelegramClient
 from config.env import API_ID, API_HASH, SESSION_NAME
 
-client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
+client = TelegramClient(
+    SESSION_NAME,
+    API_ID,
+    API_HASH,
+    connection_retries=5,
+    retry_delay=5,
+    auto_reconnect=True
+)
