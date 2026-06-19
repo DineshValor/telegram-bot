ac208442c10a7f6bb61d998d71665360d94511a7
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2026-06-19 13:39:12 +0530

    Update client.py

--- core/client.py
+++ core/client.py
@@ -1,11 +1,4 @@
 from telethon import TelegramClient
 from config.env import API_ID, API_HASH, SESSION_NAME
 
-client = TelegramClient(
-    SESSION_NAME,
-    API_ID,
-    API_HASH,
-    connection_retries=5,
-    retry_delay=5,
-    auto_reconnect=True
-)
+client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
