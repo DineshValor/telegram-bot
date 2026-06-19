2810d12670f9953505240eb09847a745590aa484
Dinesh Valor <98645523+DineshValor@users.noreply.github.com>
2026-06-19 16:17:43 +0530

    Implement MTProto client creation with proxy
    Added functions to create MTProto client with proxy support.

--- core/client.py
+++ core/client.py
@@ -1,10 +1,16 @@
 from telethon import TelegramClient
 
+from telethon.network.connection import (
+    ConnectionTcpMTProxyRandomizedIntermediate
+)
+
 from config.env import (
     API_ID,
     API_HASH,
     SESSION_NAME,
 )
+
+from core.proxy_manager import proxy_manager
 
 _client = None
 
@@ -15,6 +21,37 @@ def create_direct_client():
         API_ID,
         API_HASH,
     )
+
+
+# ADD BELOW create_direct_client()
+
+def create_mtproto_client(proxy):
+
+    return TelegramClient(
+        SESSION_NAME,
+        API_ID,
+        API_HASH,
+        connection=(
+            ConnectionTcpMTProxyRandomizedIntermediate
+        ),
+        proxy=(
+            proxy["host"],
+            proxy["port"],
+            proxy["secret"]
+        )
+    )
+
+
+def create_proxy_client():
+
+    proxy = proxy_manager.get_next_proxy()
+
+    if not proxy:
+        return None, None
+
+    client = create_mtproto_client(proxy)
+
+    return client, proxy
 
 
 def get_client():
